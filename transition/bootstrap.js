0c5826c756a20c51311fbb4311a7449677bb45f5
Doug Thayer <dougathayer@gmail.com>
2017-04-28 10:16:02 -0700

    Updated xpis and created transition directory
    - gecko_profiler.xpi just points to the old SDK-based add-on.
    - gecko_profiler-0.1.xpi points to the new WebExtension.
    
    In order to transition, we'll just need to pull the files from
    the transition folder into the root folder, overwriting the
    update.rdf file with the one from transition.

---
+++ transition/bootstrap.js
@@ -0,0 +1,32 @@
+// NOTE: this file's only purpose is to be packaged into gecko_profiler_shim.xpi,
+// which will download the new WebExtension and then uninstall itself. This is
+// a bit of a hack in order to allow us to have the id "geckoprofiler@mozilla.org"
+// and still allow us to update from the old SDK-based extension.
+
+const { utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/AddonManager.jsm");
+
+const NEW_ADDON_XPI_URL = "https://raw.githubusercontent.com/devtools-html/Gecko-Profiler-Addon/master/gecko_profiler.xpi";
+const ADDON_ID = "jid0-edalmuivkozlouyij0lpdx548bc@jetpack";
+
+const installListener = {
+  onInstallEnded() {
+    AddonManager.getAddonByID(ADDON_ID, addon => {
+      addon.uninstall();
+    });
+  }
+}
+
+function startup() {
+  AddonManager.getInstallForURL(NEW_ADDON_XPI_URL, (install) => {
+    install.addListener(installListener);
+    install.install();
+  }, "application/x-xpinstall");
+}
+
+function shutdown() {}
+function install() {}
+function uninstall() {}
+
+
