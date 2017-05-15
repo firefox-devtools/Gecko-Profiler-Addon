4fcb7322ebb20d06e2d909900430029ccc9bf5da
Greg Tatum <tatum.creative@gmail.com>
2017-05-15 16:51:23 -0500

    Add a comment explaining content-home.js

--- content-home.js
+++ content-home.js
@@ -1,3 +1,8 @@
+/**
+ * This file injects a check for if the Gecko Profiler Addon is installed into
+ * perf-html.io. It does not work with the dynamically set URL, e.g. for localhost. This
+ * method is simpler and doesn't really impede local development.
+ */
 const injectScript = document.createElement('script');
 const injectFunction = () => {
   // Let perf.html know that the addon is installed.
