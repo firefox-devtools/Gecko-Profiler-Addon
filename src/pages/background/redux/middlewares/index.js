20266a929647b7877e21912f8636788963c114c6
Bryan Clark <clarkbw@gmail.com>
2017-05-25 10:39:10 -0700

    Simplify the middleware by creating a logger module

--- src/pages/background/redux/middlewares/index.js
+++ src/pages/background/redux/middlewares/index.js
@@ -1,13 +1,7 @@
 import thunk from 'redux-thunk';
 import storage from './browser.storage';
+import logger from './logger';
 
-const middlewares = [thunk];
-
-if (process.env.NODE_ENV === 'development') {
-  const { createLogger } = require('redux-logger');
-  middlewares.push(createLogger({ collapsed: true }));
-}
-
-middlewares.push(storage);
+const middlewares = [thunk, logger, storage];
 
 export default middlewares;
